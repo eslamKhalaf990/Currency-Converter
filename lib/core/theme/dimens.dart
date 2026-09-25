b2122a49e6ae38dc0dd491eb29081b0e71d7f222
ekhalaf <ekhalaf990@gmail.com>
2026-09-25 13:57:09 +0300

    build: add core theme tokens for design system

---
+++ lib/core/theme/dimens.dart
@@ -0,0 +1,16 @@
+class Dimens {
+  Dimens._();
+
+  // Spacing
+  static const double spacingXs = 4.0;
+  static const double spacingS = 8.0;
+  static const double spacingM = 16.0;
+  static const double spacingL = 24.0;
+  static const double spacingXl = 32.0;
+
+  // Radii
+  static const double radiusS = 4.0;
+  static const double radiusM = 8.0;
+  static const double radiusL = 12.0;
+  static const double radiusXl = 16.0;
+}
